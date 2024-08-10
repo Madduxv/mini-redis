2fde40934f9fd594d200df6b81723fc4416f73c3
Madduxv <madduxvjvitrano@gmail.com>
2024-08-10 14:58:08 -0500

    Implementing the RESP

--- internal/protocol/parser.go
+++ internal/protocol/parser.go
@@ -1,1 +1,35 @@
 package protocol
+
+/* RESP:
+   *x-> An array with x elements
+   $x-> A bulk string with x elements
+*/
+
+/* example Requests:
+SET: {
+  *3
+  $3
+  SET
+  $4
+  mykey
+  $13
+  Hello, World!
+}
+
+GET: {
+  *2
+  $3
+  GET
+  $4
+  mykey
+}
+
+DEL: {
+  *2
+  $3
+  DEL
+  $4
+  mykey
+}
+
+*/
