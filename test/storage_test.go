05ae9824289689161722434cbf94dc47e63a66a0
Madduxv <madduxvjvitrano@gmail.com>
2024-08-17 09:35:29 -0500

    Created tests for HRemoveStringField and HRemoveListField and fixed bug with HRemoveListField

--- test/storage_test.go
+++ test/storage_test.go
@@ -61,11 +61,47 @@ func TestHRemove(t *testing.T) {
 
   _, ok := store.StringStore[key]
   if ok {
-    t.Errorf("HRemove failed: Key still exists in StringStore")
+    t.Errorf("HRemove failed: Key '%v' still exists in StringStore", key)
   }
 
   _, ok1 := store.ListStore[key]
   if ok1 {
-    t.Errorf("HRemove failed: Key still exists in ListStore")
+    t.Errorf("HRemove failed: Key '%v' still exists in ListStore", key)
+  }
+}
+
+func TestHRemoveStringField(t *testing.T) {
+  store := storage.NewStorage()
+  defer storage.ClearStorage(store)
+
+  key := "user:1234"
+  field := "name"
+  value := "Maddux"
+
+  store.HSet(key, field, value)
+
+  store.HRemoveStringField(key, field)
+
+  val, ok := store.StringStore[key][field]
+  if ok {
+    t.Errorf("HRemoveStringField failed: Field '%v' still exists in StringStore with value '%v'",field, val)
+  }
+}
+
+func TestHRemoveListField(t *testing.T) {
+  store := storage.NewStorage()
+  defer storage.ClearStorage(store)
+
+  key := "user:1234"
+  field := "genres"
+  value := []string{"ITALIAN", "AMERICAN", "JAPANESE"}
+
+  store.HSetList(key, field, value)
+
+  store.HRemoveListField(key, field)
+
+  val, ok := store.ListStore[key][field]
+  if ok {
+    t.Errorf("HRemoveListField failed: Field '%v' still exists in ListStore with values '%v'",field, val)
   }
 }
