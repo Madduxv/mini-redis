a5067ed68c7a35bde4adbcee6829b2b843ba0ad5
Madduxv <madduxvjvitrano@gmail.com>
2024-08-22 02:15:44 -0500

    Adding RPush Tests

--- test/storage_test.go
+++ test/storage_test.go
@@ -7,101 +7,104 @@ import (
 )
 
 func TestHSet(t *testing.T) {
-  store := storage.NewStorage()
-  defer storage.ClearStorage(store)
+	store := storage.NewStorage()
+	defer storage.ClearStorage(store)
 
-  key := "user:1234"
-  field := "name"
-  value := "Maddux"
+	key := "user:1234"
+	field := "name"
+	value := "Maddux"
 
-  store.HSet(key, field, value)
+	store.HSet(key, field, value)
 
-  if store.StringStore[key][field] != value {
-    t.Errorf("HSet failed, expected %v, got %v", value, store.StringStore[key][field])
-  }
+	if store.StringStore[key][field] != value {
+		t.Errorf("HSet failed, expected %v, got %v", value, store.StringStore[key][field])
+	}
+}
+
+func TestRPush(t *testing.T) {
 }
 
 func TestHSetList(t *testing.T) {
-  store := storage.NewStorage()
-  defer storage.ClearStorage(store)
+	store := storage.NewStorage()
+	defer storage.ClearStorage(store)
 
-  key := "user:1234"
-  field := "genres"
-  value := []string{"ITALIAN", "AMERICAN", "JAPANESE"}
+	key := "user:1234"
+	field := "genres"
+	value := []string{"ITALIAN", "AMERICAN", "JAPANESE"}
 
-  store.HSetList(key, field, value)
+	store.HSetList(key, field, value)
 
-  if len(store.ListStore[key][field]) != len(value) {
-    t.Errorf("HSetList failed, expected list of length %d, got %d", len(value), len(store.ListStore[key][field]))
-  }
+	if len(store.ListStore[key][field]) != len(value) {
+		t.Errorf("HSetList failed, expected list of length %d, got %d", len(value), len(store.ListStore[key][field]))
+	}
 
-  for i, v := range value {
-    if store.ListStore[key][field][i] != v {
-      t.Errorf("HSetList failed, expected %v at index %d, got %v", v, i, store.ListStore[key][field][i])
-    }
-  }
+	for i, v := range value {
+		if store.ListStore[key][field][i] != v {
+			t.Errorf("HSetList failed, expected %v at index %d, got %v", v, i, store.ListStore[key][field][i])
+		}
+	}
 }
 
 func TestHRemove(t *testing.T) {
-  store := storage.NewStorage()
-  defer storage.ClearStorage(store)
+	store := storage.NewStorage()
+	defer storage.ClearStorage(store)
 
-  key := "user:1234"
-  field := "name"
-  value := "Maddux"
+	key := "user:1234"
+	field := "name"
+	value := "Maddux"
 
-  key1 := "user:1234"
-  field1 := "genres"
-  value1 := []string{"ITALIAN", "AMERICAN", "JAPANESE"}
+	key1 := "user:1234"
+	field1 := "genres"
+	value1 := []string{"ITALIAN", "AMERICAN", "JAPANESE"}
 
-  store.HSet(key, field, value)
-  store.HSetList(key1, field1, value1)
+	store.HSet(key, field, value)
+	store.HSetList(key1, field1, value1)
 
-  store.HRemove(key1)
+	store.HRemove(key1)
 
-  _, ok := store.StringStore[key]
-  if ok {
-    t.Errorf("HRemove failed: Key '%v' still exists in StringStore", key)
-  }
+	_, ok := store.StringStore[key]
+	if ok {
+		t.Errorf("HRemove failed: Key '%v' still exists in StringStore", key)
+	}
 
-  _, ok1 := store.ListStore[key]
-  if ok1 {
-    t.Errorf("HRemove failed: Key '%v' still exists in ListStore", key)
-  }
+	_, ok1 := store.ListStore[key]
+	if ok1 {
+		t.Errorf("HRemove failed: Key '%v' still exists in ListStore", key)
+	}
 }
 
 func TestHRemoveStringField(t *testing.T) {
-  store := storage.NewStorage()
-  defer storage.ClearStorage(store)
+	store := storage.NewStorage()
+	defer storage.ClearStorage(store)
 
-  key := "user:1234"
-  field := "name"
-  value := "Maddux"
+	key := "user:1234"
+	field := "name"
+	value := "Maddux"
 
-  store.HSet(key, field, value)
+	store.HSet(key, field, value)
 
-  store.HRemoveStringField(key, field)
+	store.HRemoveStringField(key, field)
 
-  val, ok := store.StringStore[key][field]
-  if ok {
-    t.Errorf("HRemoveStringField failed: Field '%v' still exists in StringStore with value '%v'",field, val)
-  }
+	val, ok := store.StringStore[key][field]
+	if ok {
+		t.Errorf("HRemoveStringField failed: Field '%v' still exists in StringStore with value '%v'", field, val)
+	}
 }
 
 func TestHRemoveListField(t *testing.T) {
-  store := storage.NewStorage()
-  defer storage.ClearStorage(store)
+	store := storage.NewStorage()
+	defer storage.ClearStorage(store)
 
-  key := "user:1234"
-  field := "genres"
-  value := []string{"ITALIAN", "AMERICAN", "JAPANESE"}
+	key := "user:1234"
+	field := "genres"
+	value := []string{"ITALIAN", "AMERICAN", "JAPANESE"}
 
-  store.HSetList(key, field, value)
+	store.HSetList(key, field, value)
 
-  store.HRemoveListField(key, field)
+	store.HRemoveListField(key, field)
 
-  val, ok := store.ListStore[key][field]
-  if ok {
-    t.Errorf("HRemoveListField failed: Field '%v' still exists in ListStore with values '%v'",field, val)
-  }
+	val, ok := store.ListStore[key][field]
+	if ok {
+		t.Errorf("HRemoveListField failed: Field '%v' still exists in ListStore with values '%v'", field, val)
+	}
 }
